eaed2de7576c3210ad1740c4b4813b855f07eca3
Neeal20 <nlahiany@gmail.com>
2022-12-23 15:38:13 +0100

    Fix : bug hebergement

--- router.js
+++ router.js
@@ -15,34 +15,34 @@ const router = express.Router();
 router.get("/", loginController.getPseudoSession, mainController.renderHomePage);
 
 // AFficher tout les Pokémons de la Db
-router.get("/pokedex", loginController.getPseudoSession, mainController.renderPokedexPage);
+router.get("/pokedex", mainController.renderPokedexPage);
 
 // Afficher l'information d'un Pokémon via son ID
-router.get("/pokedex/:id", loginController.getPseudoSession, mainController.renderSinglePokemon);
+router.get("/pokedex/:id", mainController.renderSinglePokemon);
 
 // Connection de l'utilisateur sur le site
-router.get("/login",loginController.getPseudoSession, loginController.loginPage);
+router.get("/login", loginController.loginPage);
 
 // Contrôle de l'authentification / Eviter les doublons dans la Db
-router.post("/auth", loginController.getPseudoSession, loginController.loginVerification);
+router.post("/auth", loginController.loginVerification);
 
 // Afficher la page login
-router.get("/signin", loginController.getPseudoSession, loginController.renderSignPage);
+router.get("/signin", loginController.renderSignPage);
 
 // Déconnection de l'utilisateur
-router.get("/logout", loginController.getPseudoSession, loginController.logoutSession);
+router.get("/logout", loginController.logoutSession);
 
 // Inscription utilisateur dans la Db
-router.post("/signin", loginController.getPseudoSession, loginController.loginPost);
+router.post("/signin", loginController.loginPost);
 
 // Fonction de recherche de pokémon par type
-router.get("/search/pokemon/type", loginController.getPseudoSession, searchController.searchPokemonByType);
+router.get("/search/pokemon/type", searchController.searchPokemonByType);
 
 // Fonction de recherche de pokémon par nom
-router.get("/search/pokemon/name", loginController.getPseudoSession, searchController.searchPokemonByName);
+router.get("/search/pokemon/name", searchController.searchPokemonByName);
 
 // Affichage de la page des favoris
-router.get("/favoris", loginController.getPseudoSession, bookmarksController.renderBookmarks);
+router.get("/favoris", bookmarksController.renderBookmarks);
 
 // Export du module router
 module.exports = router;
